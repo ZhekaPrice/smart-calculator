b1137b68e01db0e613e642217757e3e91338b8a8
ZhekaPrice <36643027+ZhekaPrice@users.noreply.github.com>
2018-03-12 20:23:41 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.value = initialValue.toString();
   }
 
   add(number) {
-    // your implementation
+    this.value += '+' + number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.value += '-' + number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.value += '*' + number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.value += '/' + number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.value += '**' + number;
+    return this;
+  }
+
+  valueOf()
+  {
+    return eval(this.value);
   }
 }
 
